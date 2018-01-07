d87f453d3cf16a180ea4d93c8a29f444059cf84d
Koen <koen@emotion.nl>
2018-01-07 09:00:40 +0100

    Build our own bundle.js

--- src/js/app.js
+++ src/js/app.js
@@ -1,4 +1,105 @@
-// JS Goes here - ES6 supported
+import 'jquery'
+import 'bootstrap'
+import Typed from 'typed.js';
+import {WOW} from 'wowjs';
 
-// Say hello
-console.log("🦊 Hello! Edit me in src/js/app.js");
+(function ($) {
+  'use strict'
+  var app = {
+    // Inverse header background when scroll
+    hederInverse: function () {
+      $(window).on('scroll', function () {
+        if ($(this).scrollTop() > 5) {
+          $('body').addClass('is-scrolling')
+        } else {
+          $('body').removeClass('is-scrolling')
+        }
+      })
+    },
+
+    accordianToggleIcon: function () {
+      $('.accordion__title').on('click', function () {
+        $('.accordion__title.active').removeClass('active')
+        $(this).addClass('active')
+      })
+    },
+
+    // Smooth scroll to target element
+
+    scrollTo: function () {
+      $('[data-scrollto]').on('click', function () {
+        var id = '#' + $(this).data('scrollto')
+        if ($(id).length > 0) {
+          var offset = 0
+          if ($('.header').length) {
+            offset = $('.header').height()
+          }
+          $('html').animate({scrollTop: $(id).offset().top - offset}, 700)
+        }
+        return false
+      })
+    },
+
+    /// Scroll to top
+    scrollTop: function () {
+      var documentOffsetHeight = document.body.offsetHeight
+      var windowHeight = $(window).height()
+      $(window).on('scroll', function () {
+        if ($(this).scrollTop() > (windowHeight + 500)) {
+          $('.scroll-top.active').removeClass('active')
+          $('.scroll-top').addClass('active')
+        } else {
+          $('.scroll-top').removeClass('active')
+        }
+      })
+      $('.scroll-top').on('click', function () {
+        $('html, body').animate({
+          scrollTop: 0
+        }, 600)
+        return false
+      })
+    },
+
+    wowInit: function () {
+      var wow = new WOW(
+        {
+          boxClass: 'wow',
+          animateClass: 'animated',
+          offset: 0,
+          mobile: true,
+          live: true,
+          scrollContainer: null
+        }
+      )
+      wow.init()
+    },
+
+    typingText: function () {
+      if ($('[data-type]').data('type') === undefined) {
+        return false
+      }
+      var typedTxt = $('[data-type]').data('type').split(',')
+      var typed = new Typed('[data-type]', {
+        strings: typedTxt,
+        typeSpeed: 80,
+        loop: true,
+        backSpeed: 80,
+        showCursor: false
+      })
+    },
+
+    // Init the main function
+    init: function () {
+      app.hederInverse()
+      app.accordianToggleIcon()
+      app.scrollTo()
+      app.scrollTop()
+      app.wowInit()
+      app.typingText()
+    }
+
+  }
+
+  app.init()
+
+}(jQuery))
--- webpack.conf.js
+++ webpack.conf.js
@@ -1,37 +1,39 @@
-import webpack from "webpack";
-import path from "path";
+import webpack from 'webpack'
+import path from 'path'
 
 export default {
   module: {
     rules: [
       {
         test: /\.((png)|(eot)|(woff)|(woff2)|(ttf)|(svg)|(gif))(\?v=\d+\.\d+\.\d+)?$/,
-        loader: "file-loader?name=/[hash].[ext]"
+        loader: 'file-loader?name=/[hash].[ext]'
       },
-      {test: /\.json$/, loader: "json-loader"},
+      {test: /\.json$/, loader: 'json-loader'},
       {
-        loader: "babel-loader",
+        loader: 'babel-loader',
         test: /\.js?$/,
         exclude: /node_modules/,
         query: {cacheDirectory: true}
       }
-    ]
+    ],
   },
-
+  devtool: 'source-map',
   plugins: [
     new webpack.ProvidePlugin({
-      "fetch": "imports-loader?this=>global!exports?global.fetch!whatwg-fetch"
+      $: 'jquery',
+      jQuery: 'jquery',
+      'window.jQuery': 'jquery',
+    }),
+    new webpack.optimize.UglifyJsPlugin({
+      minimize: true
     })
   ],
-
-  context: path.join(__dirname, "src"),
   entry: {
-    app: ["./js/app"]
+    app: './src/js/app',
   },
   output: {
-    path: path.join(__dirname, "dist"),
-    publicPath: "/",
-    filename: "[name].js"
-  },
-  externals:  [/^vendor\/.+\.js$/]
-};
+    path: path.join(__dirname, 'dist', 'js'),
+    publicPath: '/',
+    filename: 'bundle.js'
+  }
+}
